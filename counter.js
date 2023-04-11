3936e09a365bdd00223f692d3e8a804fefa95890
Roshnicodes88 <106426143+Roshnicodes88@users.noreply.github.com>
2023-04-11 19:54:38 +0545

    Increment and Decrement Counter.
    This is a increment and Decrement Counter made by using HTML, CSS and JavaScript.

---
+++ counter.js
@@ -0,0 +1,12 @@
+var data = 0;
+document.getElementById("box").innerText=data;
+function decrement(){
+    data = data-1;
+    document.getElementById("box").innerText=data;
+    
+}
+function increment(){
+    data = data+1;
+    document.getElementById("box").innerText=data;
+    
+}
